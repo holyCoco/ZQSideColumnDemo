0f4b101bb5cc61164dd2bdf49c05c5d9943f3c8b
Carmack <925324664@qq.com>
2016-01-07 16:50:26 +0800

    Initial Commit

---
+++ ZQSideColumnDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZQSideColumnDemo
+//
+//  Created by zq on 16/1/7.
+//  Copyright © 2016年 zhangqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
